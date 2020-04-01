e834983465b9f97425e2d9b7d439019a7a58f22b
Daniel Sheppard <daniel.sheppard@md.catapult.org.uk>
2020-04-01 14:20:28 +0100

    Upgrade to Scala 2.13 and Tika (via doclib-common).

--- src/main/scala/io/mdcatapult/doclib/consumers/ConsumerRawText.scala
+++ src/main/scala/io/mdcatapult/doclib/consumers/ConsumerRawText.scala
@@ -1,31 +1,38 @@
 package io.mdcatapult.doclib.consumers
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.Materializer
 import com.spingo.op_rabbit.SubscriptionRef
 import io.mdcatapult.doclib.consumer.AbstractConsumer
 import io.mdcatapult.doclib.handlers.RawTextHandler
 import io.mdcatapult.doclib.messages._
 import io.mdcatapult.doclib.models.DoclibDoc
 import io.mdcatapult.klein.mongo.Mongo
-import io.mdcatapult.klein.queue.Queue
+import io.mdcatapult.klein.queue.{Envelope, Queue}
 import org.mongodb.scala.MongoCollection
-
-import scala.concurrent.ExecutionContextExecutor
+import play.api.libs.json.Format
 
 /**
   * RabbitMQ Consumer to extract files to raw text
   */
 object ConsumerRawText extends AbstractConsumer("consumer-unarchive") {
 
-  def start()(implicit as: ActorSystem, materializer: ActorMaterializer, mongo: Mongo): SubscriptionRef = {
-    implicit val ex: ExecutionContextExecutor = as.dispatcher
+  override def start()(implicit as: ActorSystem, m: Materializer, mongo: Mongo): SubscriptionRef = {
+    import as.dispatcher
+
     implicit val collection: MongoCollection[DoclibDoc] = mongo.database.getCollection(config.getString("mongo.collection"))
 
+    def queue[T <: Envelope](property: String)(implicit f: Format[T]): Queue[T] =
+      new Queue[T](config.getString(property), consumerName = Some("rawtext"))
+
     /** initialise queues **/
-    val downstream: Queue[PrefetchMsg] = new Queue[PrefetchMsg](config.getString("downstream.queue"), Some("rawtext"))
-    val upstream: Queue[DoclibMsg] = new Queue[DoclibMsg](config.getString("upstream.queue"), Some("rawtext"))
-    val supervisor: Queue[SupervisorMsg] = new Queue[SupervisorMsg](config.getString("doclib.supervisor.queue"), Some("rawtext"))
-    upstream.subscribe(new RawTextHandler(downstream, supervisor).handle, config.getInt("upstream.concurrent"))
+    val downstream: Queue[PrefetchMsg] = queue("downstream.queue")
+    val upstream: Queue[DoclibMsg] = queue("upstream.queue")
+    val supervisor: Queue[SupervisorMsg] = queue("doclib.supervisor.queue")
+
+    upstream.subscribe(
+      new RawTextHandler(downstream, supervisor).handle,
+      config.getInt("upstream.concurrent")
+    )
   }
 }
--- src/main/scala/io/mdcatapult/doclib/handlers/RawTextHandler.scala
+++ src/main/scala/io/mdcatapult/doclib/handlers/RawTextHandler.scala
@@ -1,15 +1,14 @@
 package io.mdcatapult.doclib.handlers
 
-import akka.actor.ActorSystem
 import cats.data._
 import cats.implicits._
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import io.mdcatapult.doclib.messages._
-import io.mdcatapult.klein.queue.Sendable
-import io.mdcatapult.doclib.models.{Derivative, DoclibDoc, Origin}
 import io.mdcatapult.doclib.models.metadata.{MetaString, MetaValueUntyped}
+import io.mdcatapult.doclib.models.{Derivative, DoclibDoc, Origin}
 import io.mdcatapult.doclib.util.DoclibFlags
+import io.mdcatapult.klein.queue.Sendable
 import io.mdcatapult.rawtext.extractors.RawText
 import org.bson.types.ObjectId
 import org.mongodb.scala.MongoCollection
@@ -17,13 +16,11 @@ import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.model.Updates._
 import org.mongodb.scala.result.UpdateResult
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
-
 class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[SupervisorMsg])
-                    (implicit ac: ActorSystem,
-                     ex: ExecutionContextExecutor,
+                    (implicit ex: ExecutionContext,
                      config: Config,
                      collection: MongoCollection[DoclibDoc]
                     ) extends LazyLogging {
@@ -33,26 +30,25 @@ class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[Super
   def handle(msg: DoclibMsg, key: String): Future[Option[Any]] = {
     logger.info(f"RECEIVED: ${msg.id}")
     (for {
-      doc ← OptionT(fetch(msg.id))
-      started: UpdateResult ← OptionT(flags.start(doc))
+      doc <- OptionT(fetch(msg.id))
+      started: UpdateResult <- OptionT(flags.start(doc))
       // TODO - validate mimetype here??
-      newFilePath ← OptionT(extractRawText(doc.source))
-      persisted ← OptionT(persist(doc, newFilePath))
-      _ ← OptionT(enqueue(newFilePath, doc))
-      _ ← OptionT(flags.end(doc, started.getModifiedCount > 0))
+      newFilePath <- OptionT(extractRawText(doc.source))
+      persisted <- OptionT(persist(doc, newFilePath))
+      _ <- OptionT(enqueue(newFilePath, doc))
+      _ <- OptionT(flags.end(doc, noCheck = started.getModifiedCount > 0))
     } yield (newFilePath, persisted, doc)).value.andThen({
 
-      case Success(result) ⇒ result match {
-        case Some(r) ⇒ {
+      case Success(result) => result match {
+        case Some(r) =>
           supervisor.send(SupervisorMsg(id = r._3._id.toHexString))
           logger.info(f"COMPLETE: ${msg.id} - converted to raw text - ${r._1}")
-        }
-        case None ⇒ logger.info(f"${msg.id} - no document found")
+        case None => logger.info(f"${msg.id} - no document found")
       }
-      case Failure(err) ⇒ OptionT(fetch(msg.id)).value.andThen({
-        case Success(result) ⇒ result match {
-          case Some(foundDoc) ⇒ flags.error(foundDoc, noCheck = true)
-          case None => //Do nothing. The error is bubbling up. There is no mongo doc to set flags on
+      case Failure(_) => OptionT(fetch(msg.id)).value.andThen({
+        case Success(result) => result match {
+          case Some(foundDoc) => flags.error(foundDoc, noCheck = true)
+          case None => () //Do nothing. The error is bubbling up. There is no mongo doc to set flags on
         }
       })
     })
@@ -88,7 +84,7 @@ class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[Super
   def extractRawText(source: String): Future[Option[String]] =
     Try(new RawText(source).extract) match {
       case Success(r) => Future.successful(Some(r))
-      case Failure(e) ⇒ throw e
+      case Failure(e) => throw e
     }
 
   def fetch(id: String): Future[Option[DoclibDoc]] =
