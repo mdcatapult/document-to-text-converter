baf5104d9db4ac79a689a8183cf0a2cd7a1ca6c7
Michael Sweeton <michael.sweeton@md.catapult.org.uk>
2021-01-08 11:38:11 +0000

    logs error if can't write error flag, or can't find document

--- src/main/scala/io/mdcatapult/doclib/handlers/RawTextHandler.scala
+++ src/main/scala/io/mdcatapult/doclib/handlers/RawTextHandler.scala
@@ -1,27 +1,26 @@
 package io.mdcatapult.doclib.handlers
 
-import java.util.UUID
-
 import cats.data._
 import cats.implicits._
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import io.mdcatapult.doclib.ConsumerName
+import io.mdcatapult.doclib.flag.{FlagContext, MongoFlagStore}
 import io.mdcatapult.doclib.messages._
+import io.mdcatapult.doclib.metrics.Metrics.handlerCount
 import io.mdcatapult.doclib.models.metadata.{MetaString, MetaValueUntyped}
 import io.mdcatapult.doclib.models.{DoclibDoc, DoclibDocExtractor, Origin, ParentChildMapping}
-import io.mdcatapult.doclib.flag.{FlagContext, MongoFlagStore}
 import io.mdcatapult.klein.queue.Sendable
 import io.mdcatapult.rawtext.extractors.RawText
-import io.mdcatapult.util.time.nowUtc
-import io.mdcatapult.doclib.metrics.Metrics.handlerCount
 import io.mdcatapult.util.models.Version
 import io.mdcatapult.util.models.result.UpdatedResult
+import io.mdcatapult.util.time.nowUtc
 import org.bson.types.ObjectId
 import org.mongodb.scala.MongoCollection
 import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.result.InsertManyResult
 
+import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
@@ -45,7 +44,8 @@ class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[Super
   def handle(msg: DoclibMsg, key: String): Future[Option[Any]] = {
     logger.info(f"RECEIVED: ${msg.id}")
     val flagContext: FlagContext = flags.findFlagContext(Some(config.getString("upstream.queue")))
-    (for {
+
+    val rawTextProcess = for {
       doc <- OptionT(fetch(msg.id))
       if !docExtractor.isRunRecently(doc)
       started: UpdatedResult <- OptionT.liftF(flagContext.start(doc))
@@ -54,8 +54,9 @@ class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[Super
       persisted <- OptionT(persist(doc, newFilePath))
       _ <- OptionT(enqueue(newFilePath, doc))
       _ <- OptionT.liftF(flagContext.end(doc, noCheck = started.changesMade))
-    } yield (newFilePath, persisted, doc)).value.andThen({
+    } yield (newFilePath, persisted, doc)
 
+    rawTextProcess.value.andThen {
       case Success(result) => result match {
         case Some(r) =>
           supervisor.send(SupervisorMsg(id = r._3._id.toHexString))
@@ -65,15 +66,20 @@ class RawTextHandler(prefetch: Sendable[PrefetchMsg], supervisor: Sendable[Super
           logger.info(f"${msg.id} - no document found")
           handlerCount.labels(ConsumerName, config.getString("upstream.queue"), "error_no_document").inc()
       }
-      case Failure(_) =>
+      case Failure(e) =>
+        logger.error("error during handle process", e)
         handlerCount.labels(ConsumerName, config.getString("upstream.queue"), "unknown_error").inc()
-        OptionT(fetch(msg.id)).value.andThen({
-        case Success(result) => result match {
-          case Some(foundDoc) => flagContext.error(foundDoc, noCheck = true)
-          case None => () //Do nothing. The error is bubbling up. There is no mongo doc to set flags on
+
+        fetch(msg.id).map {
+          case Some(foundDoc) =>
+            flagContext.error(foundDoc, noCheck = true).andThen {
+              case Failure(e) => logger.error("error attempting error flag write", e)
+            }
+          case None =>
+            val message = s"could not find ${msg.id}"
+            logger.error(message, new Exception(message))
         }
-      })
-    })
+    }
   }
 
   def enqueue(newFilePath: String, doc: DoclibDoc): Future[Option[Boolean]] = {
